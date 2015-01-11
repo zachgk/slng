008570326435a3aaf9094365a2f8f697b3d06126
Zach Kimberg <zachary@kimberg.com>
2015-01-11 00:09:36 -0600

    Start new hypergraph implementation

---
+++ transpiler/newGraph.py
@@ -0,0 +1,149 @@
+from collections import deque, namedtuple
+from expr import *
+from common import *
+import re
+
+
+def neighborEdges(nodeRef):
+    parentage = set()
+    path = tuple()
+    for refParent in nodeRef.parents:
+        path = path + (refParent,)
+        nr = NodeRef(nodeRef.node,path)
+        parentage = parentage.union({e for e in refParent.graph.edges if nr in e.nodes})
+    ne = {e for e in nodeRef.node.graph.edges if nodeRef.node in e.nodes}
+    return parentage.union(ne)
+
+def absEdge(edge, path):
+    nodes = set()
+    for n in edge.nodes:
+        if type(n) is NodeRef:
+            nodes.add(n)
+        elif type(n) is Node:
+            nodes.add(NodeRef(n,path))
+        else:
+            Error("unknown type of node for neighbors: " + str(type(n)))
+    return edge._replace(nodes=frozenset(nodes))
+
+def absNeighborEdges(nodeRef):
+    parentage = set()
+    path = tuple(nodeRef.parents)
+    for i in range(len(path)):
+        last = path[-1]
+        path = path[:-1]
+        parentage = parentage.union({absEdge(e,path) for e in last.graph.edges if nodeRef in e.nodes})
+    ne = {absEdge(e,nodeRef.parents) for e in nodeRef.node.graph.edges if nodeRef.node in e.nodes}
+    return parentage.union(ne)
+
+def neighbors(nodeRef):
+    edges = neighborEdges(nodeRef)
+    nodes = set()
+    for e in edges:
+        for n in e.nodes:
+            if type(n) is NodeRef:
+                nodes.add(n)
+            elif type(n) is Node:
+                nodes.add(NodeRef(n,nodeRef.parents))
+            else:
+                Error("unknown type of node for neighbors: " + str(type(n)))
+    nodes.discard(nodeRef)
+    return nodes
+    
+def tree(nodeRef):
+    visited = {nodeRef}
+    queue = deque([(nodeRef,[])])
+    t = {nodeRef: dict()}
+    while len(queue) > 0:
+        nr, path = queue.popleft()
+        nbrs = neighbors(nr)
+        nbrs = nbrs.difference(visited)
+        visited = visited.union(nbrs)
+        lt = t
+        for p in path: lt = lt[p]
+        lt[nr] = dict()
+        newPath = path.copy()
+        newPath.append(nr)
+        for n in nbrs:
+            queue.append((n,newPath))
+    return t
+
+def treeCompute(nodeRef):
+    t = tree(nodeRef)
+    res = treeComputeRec(nodeRef,t)
+    if res is None: Error("Could not compute " + str(nodeRef))
+    return res
+
+def treeComputeRec(root, tree):
+    ne = neighborEdges(nodeRef)
+    rootCycles = {e for e in ne if len(e.nodes)==1}
+    if len(rootCycles) > 0:
+        return list(rootCycles)[0]
+    else:
+        for r,t in tree.items():
+           rec = treeComputeRec(r,t)
+           if rec is not None:
+                edge = {e for e in ne if r.name in e[0] and root.name in e[0]}
+                exprs = {rec,list(edge)[0][1]}
+                subs = dict()
+                if type(r.graph) is Subgraph: subs[r.name] = r.graph.name + "." + r.name
+                if type(root.graph) is Subgraph: subs[root.name] = root.graph.name + "." + root.name
+                return exprParser.solve(exprs,root,subs=subs)
+    return None
+
+
+class Hypergraph:
+    def __init__(self):
+        self.nodes = set()
+        self.edges = set()
+
+Node = namedtuple('Node', 'graph name')
+Edge = namedtuple('Edge', 'nodes equation')
+NodeRef = namedtuple('NodeRef', 'node parents')
+NodeGraph = namedtuple('NodeGraph','graph parent')
+RefParent = namedtuple('RefParent','graph nodeGraph')
+
+h = Hypergraph()
+a = Node(graph=h,name='a')
+b = Node(graph=h,name='b')
+c = Node(graph=h,name='c')
+d = Node(graph=h,name='d')
+h.nodes.add(a)
+h.nodes.add(b)
+h.nodes.add(c)
+h.nodes.add(d)
+
+e1 = Edge(nodes=frozenset({a,b}),equation='a=2*b')
+e2 = Edge(nodes=frozenset({a,c}),equation='c=4*a')
+e3 = Edge(nodes=frozenset({b,d}),equation='d=4*b')
+h.edges.add(e1)
+h.edges.add(e2)
+h.edges.add(e3)
+
+i = Hypergraph()
+j = Hypergraph()
+x = Node(graph=i,name='x')
+y = Node(graph=i,name='y')
+z = Node(graph=i,name='z')
+e4 = Edge(nodes=frozenset({x,y}),equation='x=9*y')
+e5 = Edge(nodes=frozenset({x,z}),equation='x=2*z')
+i.edges.add(e4)
+i.edges.add(e5)
+
+m = Node(graph=j,name='m')
+n = Node(graph=j,name='n')
+e6 = Edge(nodes=frozenset({m,n}),equation='m=3*n')
+j.edges.add(e6)
+
+xyz = NodeGraph(graph=i,parent=h)
+e7 = Edge(nodes=frozenset({a,NodeRef(node=x,parents=(RefParent(h,xyz),))}),equation='a=2*x')
+h.nodes.add(xyz)
+h.edges.add(e7)
+
+selfEdge = Edge(nodes=frozenset({z}),equation='z=5')
+i.edges.add(selfEdge)
+
+aRef = NodeRef(node=a,parents=tuple() )
+# print(absNeighborEdges(aRef))
+
+xRef = NodeRef(node=x,parents=(RefParent(h,xyz),))
+print(absNeighborEdges(xRef))
